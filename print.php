f80923c5e5fb7c97479ca113d7a729ab557e15e4
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-25 14:51:18 +0100

    added form

--- print.php
+++ print.php
@@ -1,37 +1,40 @@
 <?php
 
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+
+// echo $paragraph;
+// echo '<br>';
+
+$new_paragraph = str_replace($badword, '***', $paragraph);
+// echo $new_paragraph;
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>PHP Badword</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
 </head>
+
 <body>
     <div class="container mt-5">
-        <h1>Starting PHP</h1>
+        <h3>Original paragraph</h3>
+        <p><?php echo $paragraph; ?></p>
+        <div class="mb-4">Lunghezza: <?php echo strlen($paragraph); ?> caratteri.</div>
 
-        <form action="print.php" method="GET">
-            <div class="mb-3">
-              <label for="paragraph" class="form-label">Paragraph</label>
-              <input type="text" name="paragraph" id="paragraph" class="form-control" placeholder="type your paragraph here">
-            </div>
-
-            <div class="mb-3">
-              <label for="badword" class="form-label">Badword</label>
-              <input type="text" name="badword" id="badword" class="form-control" placeholder="type your badword here">
-            </div>
-
-            <button type="submit" class="btn btn-primary mt-3">Submit</button>
-        </form>
-
+        <h3>Censored paragraph</h3>
+        <p><?php echo $new_paragraph; ?></p>
+        <div class="mb-4">Lunghezza: <?php echo strlen($new_paragraph); ?> caratteri.</div>
     </div>
 
     <!-- Bootstrap JS -->
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js' integrity='sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8' crossorigin='anonymous'></script>
 </body>
+
 </html>
