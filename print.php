1d27cfdc30925eb4f960efa2fcd4c51b6b1749d2
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-25 15:04:48 +0100

    added if control on input

--- print.php
+++ print.php
@@ -6,7 +6,12 @@ $badword = trim($_GET['badword']);
 // echo $paragraph;
 // echo '<br>';
 
-$new_paragraph = str_replace($badword, '***', $paragraph);
+if ($paragraph === '') {
+    $paragraph = 'Non hai inserito testo!';
+    $new_paragraph = 'Non hai inserito testo!';
+} else {
+    $new_paragraph = str_replace($badword, '***', $paragraph);
+}
 // echo $new_paragraph;
 
 ?>
@@ -18,7 +23,7 @@ $new_paragraph = str_replace($badword, '***', $paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badword</title>
+    <title>PHP Badwords</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
 </head>
 
